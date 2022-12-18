122c0ad24742250c4b6b3aa13d5d13d34221c86d
Prajwal Acharya <mail2prajwal12@gmail.com>
2022-12-18 19:43:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,31 +2,42 @@ const axios = require('axios');
 const filesystem = require('fs');
 
 let json;
-try {
-    let jsonString = filesystem.readFileSync('extracted_data.json', 'utf8');
-    json = JSON.parse(jsonString);
-} catch (error) {
-    console.log('error reading file extracted_data.json: ', error);
+
+readJSONFile();
+callAPODApi();
+writeToJSONFile();
+
+function readJSONFile() {
+    try {
+        let jsonString = filesystem.readFileSync('extracted_data.json', 'utf8');
+        json = JSON.parse(jsonString);
+    } catch (error) {
+        console.log('error reading file extracted_data.json: ', error);
+    }
 }
 
+function writeToJSONFile() {
+    try {
+        filesystem.writeFileSync('extracted_data.json', JSON.stringify(json, null, 2),
+            'utf8', function (err) {
+                console.log(response.data);
+            });
+    } catch (error) {
+        console.log('error writing to file extracted_data.json: ', error);
+    }
+}
 
-axios.get('https://apod.ellanan.com/api', { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
-    .then(response => {
-        json.push(response.data);
-    })
-    .catch(function (error) {
-        // handle error
-        console.log(error);
-    })
-    .finally(function () {
-        console.log('finally block executed!')
-    });
-
-try {
-    filesystem.writeFileSync('extracted_data.json', JSON.stringify(json, null, 2),
-        'utf8', function (err) {
+function callAPODApi() {
+    axios.get('https://apod.ellanan.com/api', { headers: { "Accept-Encoding": "gzip,deflate,compress" } })
+        .then(response => {
             console.log(response.data);
+            json.push(response.data);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function () {
+            console.log('finally block executed!')
         });
-} catch (error) {
-    console.log('error writing to file extracted_data.json: ', error);
 }
