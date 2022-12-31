a401a039e190c6a97ec91f00781ebfde6c30369c
Prajwal Acharya <mail2prajwal12@gmail.com>
2022-12-31 07:07:51 +0530

    print file name to verify

--- src/basic.py
+++ src/basic.py
@@ -10,6 +10,7 @@ consolidated_data = []
 path_to_json = 'extracted_data/'
 for file_name in [file for file in os.listdir(path_to_json) if file.endswith('.json')]:
   with open(path_to_json + file_name) as json_file:
+    print(path_to_json + file_name)
     data = json.load(json_file)
     consolidated_data.append(data)
 
