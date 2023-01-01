bfe359d2559a4dbe2bde005867115c031734dc0d
Prajwal Acharya <mail2prajwal12@gmail.com>
2023-01-01 20:22:03 +0530

    test python script and update to readme

--- src/basic.py
+++ src/basic.py
@@ -6,13 +6,10 @@ response = requests.get(baseURL)
 print(response.status_code)
 print(response.json())
 
-consolidated_data = []
-path_to_json = 'extracted_data/'
-for file_name in [file for file in os.listdir(path_to_json) if file.endswith('.json')]:
-  with open(path_to_json + file_name) as json_file:
-    print(path_to_json + file_name)
-    data = json.load(json_file)
-    consolidated_data.append(data)
+with open(json_file) as json_file:
+    json_decoded = json.load(json_file)
+    json_decoded["apod"].append(response.json())
 
-with open('consolidated.json', 'w', encoding='utf-8') as f:
-    json.dump(consolidated_data, f, ensure_ascii=False, indent=4)
+
+with open('test.json', 'w', encoding='utf-8') as f:
+    json.dump(json_decoded, f, ensure_ascii=False, indent=4)
