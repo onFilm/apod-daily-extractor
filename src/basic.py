b2a605106cf33462f3ad793fe199d49c875dfbd6
Prajwal Acharya <mail2prajwal12@gmail.com>
2023-01-01 22:23:14 +0530

    Update basic.py

--- src/basic.py
+++ src/basic.py
@@ -5,7 +5,7 @@ with open("consolidated.json") as json_file:
 
 dir_name = 'extracted_data/'
 for file_name in [file for file in sorted(filter(lambda x: os.path.isfile(os.path.join(dir_name, x)), os.listdir(dir_name))) if file.endswith('.json')]:
-  if os.stat(file_name).st_size != 0:
+  if os.stat(dir_name + file_name).st_size != 0:
     with open(dir_name + file_name) as json_file:
       data = json.load(json_file)
       json_decoded["apod"].append(data)
